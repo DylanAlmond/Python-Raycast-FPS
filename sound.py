073e300199f99b57b3853020e1877bc6eb799bf0
Dylan Almond <dylan@dylanalmond.net>
2025-05-04 16:32:28 +0100

    add screenshots + remove soundtrack files
    - Removed copyrighted soundtrack
    - Missing soundtrack files will now be ignored by the game

--- sound.py
+++ sound.py
@@ -1,26 +1,31 @@
-import pygame as pg
 import os
+import pygame as pg
 from settings import *
 
+
 class Sound:
-    def __init__(self, game):
-        self.game = game
-        pg.mixer.stop()
-        pg.mixer.init()
-        self.path = SOUND_PATH
+  def __init__(self, game):
+    self.game = game
+    pg.mixer.stop()
+    pg.mixer.init()
+    self.path = SOUND_PATH
 
-        pg.mixer.set_num_channels(CH_MAX)  # default is 8
-        self.CH_PLY = pg.mixer.Channel(0)    # player sounds
-        self.CH_WPN = pg.mixer.Channel(1)    # player weapons
-        self.CH_NPC = pg.mixer.Channel(2)    # npc
-        self.CH_NPC_W = pg.mixer.Channel(3)  # npc weapons
-        self.CH_AMB = pg.mixer.Channel(4)    # ambient sounds (doors...)
-        self.CH_MUS = pg.mixer.Channel(5)    # music
+    pg.mixer.set_num_channels(CH_MAX)  # default is 8
+    self.CH_PLY = pg.mixer.Channel(0)    # player sounds
+    self.CH_WPN = pg.mixer.Channel(1)    # player weapons
+    self.CH_NPC = pg.mixer.Channel(2)    # npc
+    self.CH_NPC_W = pg.mixer.Channel(3)  # npc weapons
+    self.CH_AMB = pg.mixer.Channel(4)    # ambient sounds (doors...)
+    self.CH_MUS = pg.mixer.Channel(5)    # music
 
-        #self.play_soundtrack(self.game.map.soundtrack)
+  def play_soundtrack(self, track):
+    full_path = os.path.join(SOUND_PATH, 'soundtrack', track)
 
-    def play_soundtrack(self):
-        try:
-            self.CH_MUS.play(pg.mixer.Sound(SOUND_PATH + 'soundtrack/' + self.game.map.soundtrack))
-        except:
-            return
+    if not os.path.isfile(full_path):
+      print(f"Soundtrack file not found: {full_path}")
+      return
+
+    try:
+      self.CH_MUS.play(pg.mixer.Sound(full_path))
+    except pg.error as e:
+      print(f"Error loading or playing soundtrack: {track}\n{e}")
